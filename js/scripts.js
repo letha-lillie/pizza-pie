0666cf75200b2c8dd1ceea37198b327c13d39d94
Katy Roberts <letha.lillie@gmail.com>
2018-11-07 20:22:30 -0800

    final project for kyle

--- js/scripts.js
+++ js/scripts.js
@@ -19,28 +19,29 @@ Pizza.prototype.totalPrice = function() {
 
 $(document).ready(function() {
   $("form#pizza_pie").submit(function(event) {
-    event.preventDefault();
-    var inputtedSize = $("input[name=size]:checked").val();
-    var inputtedProtein = $("input:checkbox[name=protein]:checked").length;
-    var inputtedVeg = $("input:checkbox[name=veg-etc]:checked").length;
-    var pizza = new Pizza(inputtedSize, inputtedProtein, inputtedVeg);
-    $("input:checkbox[name=protein]:checked").each(function() {
-      var proteinChoices = $(this).val();
+  event.preventDefault();
+  var inputtedSize = $("input[name=size]:checked").val();
+  var inputtedProtein = $("input:checkbox[name=protein]:checked").length;
+  var inputtedVeg = $("input:checkbox[name=veg-etc]:checked").length;
+  var pizza = new Pizza(inputtedSize, inputtedProtein, inputtedVeg);
 
-      $("#protein-choices").text(proteinChoices + "<br>");
-    });
-
-    $("input:checkbox[name=veg-etc]:checked").each(function() {
-      var vegChoices = $(this).val();
-
-      $("#veg-etc-choices").append(vegChoices + "<br>");
-
-    });
-
-    $("#total-price").append("$" + pizza.totalPrice());
-    $("#pizza_pie").hide();
-    $("#pizza-confirmation").show();
+  $("#protein-choices").empty()
+  $("input:checkbox[name=protein]:checked").each(function() {
+    var proteinChoices = $(this).val();
+    $("#protein-choices").append(proteinChoices + "<br>");
   });
+
+  $("#veg-etc-choices").empty()
+  $("input:checkbox[name=veg-etc]:checked").each(function() {
+    var vegChoices = $(this).val();
+    $("#veg-etc-choices").append(vegChoices + "<br>");
+  });
+
+  $("#total-price").text("$" + pizza.totalPrice());
+
+  $("#pizza_pie").hide();
+  $("#pizza-confirmation").show();
+});
   // button back to menu:
   $("button#menu-return").click(function(event){
     event.preventDefault();
